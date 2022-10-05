2439639fd7cb3aa307478e9477d2461f0b7ca1a6
Simokhin Timofei <t.simokhin@qiwi.com>
2022-10-05 13:03:48 +0300

    add city filter

--- src/main/kotlin/com/github/simohin/board/service/SheetService.kt
+++ src/main/kotlin/com/github/simohin/board/service/SheetService.kt
@@ -2,10 +2,13 @@ package com.github.simohin.board.service
 
 import com.github.simohin.board.dto.SheetRow
 import com.google.api.services.sheets.v4.Sheets
+import com.vaadin.flow.data.provider.Query
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.function.Predicate
 
 @Service
 class SheetService(
@@ -14,20 +17,44 @@ class SheetService(
     private val sheets: Sheets
 ) {
     companion object {
-        private const val RANGE = "A2:E"
+        private const val FULL_RANGE = "A2:E"
+        private const val CITY_RANGE = "D2:D"
         private val DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss")
     }
 
-    fun getAll(): List<SheetRow> =
+    @Cacheable("SheetService.findByCity")
+    fun getByCities(query: Query<SheetRow, Void>, cities: Collection<String>): List<SheetRow> {
+
+        query.page
+        query.pageSize
+
+        val sheetRows = if (cities.isEmpty()) getAll { true }
+        else getAll { cities.contains(it.city) }
+
+        if (query.sortingComparator.isEmpty) return sheetRows
+
+        return sheetRows.sortedWith(query.sortingComparator.get())
+    }
+
+    @Cacheable("SheetService.getCities")
+    fun getCities() = sheets.spreadsheets()
+        .values()
+        .get(spreadSheetId, CITY_RANGE)
+        .execute()
+        .values
+        .toDistinctValues()
+
+    private fun getAll(filter: Predicate<SheetRow>) =
         sheets.spreadsheets()
             .values()
-            .get(spreadSheetId, RANGE)
+            .get(spreadSheetId, FULL_RANGE)
             .execute()
             .values
             .toSheetRows()
+            .filter { filter.test(it) }
 
-    private fun <E> MutableCollection<E>.toSheetRows(): List<SheetRow> =
-        this.filterIsInstance<ArrayList<ArrayList<String>>>()[0].map {
+    private fun <E> MutableCollection<E>.toSheetRows() = getValues()
+        .map {
             SheetRow(
                 LocalDateTime.parse(it[0], DATETIME_FORMATTER),
                 it[1],
@@ -36,4 +63,10 @@ class SheetService(
                 it[4]
             )
         }
+
+    private fun <E> MutableCollection<E>.getValues() =
+        this.filterIsInstance<ArrayList<ArrayList<String>>>()[0]
+
+    private fun <E> MutableCollection<E>.toDistinctValues() =
+        getValues().map { it[0] }.distinct()
 }
--- src/main/kotlin/com/github/simohin/board/ui/MainLayout.kt
+++ src/main/kotlin/com/github/simohin/board/ui/MainLayout.kt
@@ -2,28 +2,26 @@ package com.github.simohin.board.ui
 
 import com.github.simohin.board.dto.SheetRow
 import com.github.simohin.board.service.SheetService
-import com.github.simohin.board.ui.util.center
+import com.vaadin.flow.component.AbstractField
 import com.vaadin.flow.component.Text
+import com.vaadin.flow.component.combobox.MultiSelectComboBox
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.grid.GridVariant
+import com.vaadin.flow.component.grid.dataview.GridLazyDataView
 import com.vaadin.flow.component.html.Anchor
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.html.Span
-import com.vaadin.flow.component.messages.MessageList
-import com.vaadin.flow.component.messages.MessageListItem
 import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
-import java.time.ZoneId
 
 @Route("")
 class MainLayout(
     private val sheetService: SheetService
 ) : VerticalLayout() {
 
-    init {
-        add(title(), messageList())
-    }
-
-    private fun title() = VerticalLayout().apply {
+    private val title = VerticalLayout().apply {
         justifyContentMode = FlexComponent.JustifyContentMode.CENTER
         alignItems = FlexComponent.Alignment.CENTER
         add(
@@ -32,24 +30,46 @@ class MainLayout(
         )
     }
 
-    private fun messageList() = MessageList().apply {
-        setHeightFull()
-        setItems(sheetService.getAll().toMessageListItems())
+    private val citiesSelector = MultiSelectComboBox<String>("Город").apply {
+        setItems(sheetService.getCities())
+        addValueChangeListener(::onSelect)
     }
 
-    private fun List<SheetRow>.toMessageListItems() = this.map {
-        MessageListItem(
-            """
-                Город: ${it.city},
-                Навык:
-                ${it.good}
-                Контакты:
-                ${it.contact}
-            """.trimIndent(),
-            it.time.atZone(ZoneId.systemDefault()).toInstant(),
-            it.name
-        ).apply {
-            center()
+    private val search = HorizontalLayout().apply {
+        justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+        alignItems = FlexComponent.Alignment.CENTER
+
+        add(citiesSelector)
+    }
+
+    private val grid = Grid(SheetRow::class.java, false).apply {
+        addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT)
+        addColumn(SheetRow::time).setHeader("Дата добавления")
+        addColumn(SheetRow::name).setHeader("Имя")
+        addColumn(SheetRow::city).setHeader("Город")
+        addColumn(SheetRow::good).setHeader("Навык")
+        addColumn(SheetRow::contact).setHeader("Контакты")
+        citiesSelector.value.map { it.toString() }
+        setItems {
+            sheetService.getByCities(
+                it,
+                citiesSelector.value.map { value -> value.toString() }
+            ).stream()
         }
     }
+
+    init {
+        add(title, search, grid)
+    }
+
+    private fun onSelect(event: AbstractField.ComponentValueChangeEvent<MultiSelectComboBox<String>, MutableSet<String>>?) =
+        doOnSelect(event!!.value)
+
+    private fun doOnSelect(cities: Collection<String>): GridLazyDataView<SheetRow>? =
+        grid.setItems {
+            sheetService.getByCities(
+                it,
+                cities
+            ).stream()
+        }
 }
